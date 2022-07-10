00fc021fbe2817036fc85059aa0dc60420ef7c95
SeoJiWon <sjiwon4491@gmail.com>
2022-07-10 23:38:20 +0900

    [2775 -> Bronze 1] 부녀회장이 될테야 (수학 / 구현)

---
+++ Bronze1/baekjoon_2775/Main.java
@@ -0,0 +1,32 @@
+package Bronze1.baekjoon_2775;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[][]arr = new int[15][15];
+        for(int i=0; i<15; i++) {
+            arr[0][i] = i;
+            arr[i][1] = 1;
+        }
+
+        for(int i=1; i<15; i++){
+            for(int j=1; j<15; j++){
+                arr[i][j] = arr[i-1][j]+arr[i][j-1];
+            }
+        }
+
+        int T = Integer.parseInt(br.readLine());
+        for(int i=0; i<T; i++){
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+            bw.write(arr[k][n]+"\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
