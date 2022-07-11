86984fa44aa97b008f8f593f0081772c6e2c33d3
SeoJiWon <sjiwon4491@gmail.com>
2022-07-11 11:56:53 +0900

    [11942 -> Bronze 5] 고려대는 사랑입니다 (구현)

---
+++ Bronze5/baekjoon_11942/Main.java
@@ -0,0 +1,15 @@
+package Bronze5.baekjoon_11942;
+
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("고려대학교");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
