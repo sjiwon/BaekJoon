95c752de5f4b2132e60a1a4193579b99a9c3d194
SeoJiWon <sjiwon4491@gmail.com>
2022-07-11 12:13:45 +0900

    [1927 -> Silver 2] 최소 힙 (자료구조 : PriorityQueue)

---
+++ Silver2/baekjoon_1927/Main.java
@@ -0,0 +1,30 @@
+package Silver2.baekjoon_1927;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        int N = Integer.parseInt(br.readLine());
+        for(int i=0; i<N; i++){
+            int x = Integer.parseInt(br.readLine());
+            if(x>0){
+                pq.offer(x);
+            }
+            else if(x==0){
+                if(pq.size() == 0)
+                    bw.write(0+"\n");
+                else
+                    bw.write(pq.poll()+"\n");
+            }
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
