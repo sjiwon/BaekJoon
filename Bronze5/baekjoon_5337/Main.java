cd4c9f831f4a5510a92c7d3e1bab407227ea312c
SeoJiWon <sjiwon4491@gmail.com>
2022-07-11 11:55:11 +0900

    [5337 -> Bronze 5] 웰컴 (구현)

---
+++ Bronze5/baekjoon_5337/Main.java
@@ -0,0 +1,18 @@
+package Bronze5.baekjoon_5337;
+
+public class Main {
+    public static void main(String[] args) {
+        print2();
+        print3();
+        print4();
+    }
+    static void print2(){
+        System.out.println(".  .   .");
+    }
+    static void print3(){
+        System.out.println("|  | _ | _. _ ._ _  _");
+    }
+    static void print4(){
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
