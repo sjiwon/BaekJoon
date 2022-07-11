d836cdf48f0bd9075850bc573474c04ac87112b2
SeoJiWon <sjiwon4491@gmail.com>
2022-07-11 11:55:18 +0900

    [5338 -> Bronze 5] 마이크로소프트 로고 (구현)

---
+++ Bronze5/baekjoon_5338/Main.java
@@ -0,0 +1,41 @@
+package Bronze5.baekjoon_5338;
+
+public class Main {
+    public static void main(String[] args){
+        print1();
+        print2();
+        print3();
+        print4();
+        print3();
+        print5();
+        print3();
+        print4();
+        print3();
+        print6();
+    }
+
+    static void print1(){
+        for(int i=0; i<7; i++)
+            System.out.print(" ");
+    }
+
+    static void print2(){
+        System.out.println("_.-;;-._");
+    }
+
+    static void print3(){
+        System.out.print("'-..-'");
+    }
+
+    static void print4(){
+        System.out.println("|   ||   |");
+    }
+
+    static void print5(){
+        System.out.println("|_.-;;-._|");
+    }
+
+    static void print6(){
+        System.out.println("|_.-''-._|");
+    }
+}
