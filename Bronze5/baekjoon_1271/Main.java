45d9ee7d36d3cb5565438844c5761064c69cd95d
SeoJiWon <sjiwon4491@gmail.com>
2022-07-11 11:53:57 +0900

    [1271 -> Bronze 5] 엄청난 부자2 (사칙연산)

---
+++ Bronze5/baekjoon_1271/Main.java
@@ -0,0 +1,22 @@
+package Bronze5.baekjoon_1271;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        BigInteger n = new BigInteger(st.nextToken());
+        BigInteger m = new BigInteger(st.nextToken());
+
+        bw.write(n.divide(m) + "\n");
+        bw.write(n.remainder(m) + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
