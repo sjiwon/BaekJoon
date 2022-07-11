2f6ec467e1998de027e262d68dc70c501d56c96c
SeoJiWon <sjiwon4491@gmail.com>
2022-07-11 11:57:35 +0900

    [15964 -> Bronze 5] 이상한 기호 (구현 / 사칙연산)

---
+++ Bronze5/baekjoon_15964/Main.java
@@ -0,0 +1,21 @@
+package Bronze5.baekjoon_15964;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+
+        bw.write(((A.add(B)).multiply(A.subtract(B))) + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
