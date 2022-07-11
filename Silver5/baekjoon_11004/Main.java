c01ae05aa649a48896f969b0b41af2653e0aa0f7
SeoJiWon <sjiwon4491@gmail.com>
2022-07-11 12:03:43 +0900

    [11004 -> Silver 5] K번째 수 (정렬)

---
+++ Silver5/baekjoon_11004/Main.java
@@ -0,0 +1,28 @@
+package Silver5.baekjoon_11004;
+
+import java.io.*;
+import java.util.*;
+public class Main {
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int [] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        bw.write(arr[K-1] + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
